a88df55370cb6b94923417a8e574ef968aaf3f64
Sebastien Galarneau <sgalarneau@gmail.com>
2016-10-03 22:42:12 -0400

    Création de Voyage (mais ca bug) et est incomplet

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <algorithm>
 
 #include "auxiliaires.h"
 #include "ligne.h"
@@ -15,31 +16,41 @@
 
 using namespace std;
 
-const std::string EMPLACEMENT_FICHIERS_RTC = "~\\RTC\\";
+const string EMPLACEMENT_FICHIERS_RTC = "~\\RTC\\";
 const char delimiteur { ',' };
+
+vector<vector<string>> fichier_voyages;
+vector<vector<string>> fichier_lignes;
+vector<vector<string>> fichier_stations;
+vector<vector<string>> fichier_arrets;
+
+/* Construction de VOYAGE */
+void initVoyages() {
+
+	for (unsigned int i = 0; i < fichier_voyages.size(); i++) {
+		for(size_t n = 0; n < fichier_lignes.size(); ++n) {
+			auto index = find(fichier_lignes[n].begin(), fichier_lignes[n].end(), fichier_voyages[i][0]);
+
+			if (fichier_lignes[n].end() != index) {
+				Ligne ligne(fichier_lignes[i]);
+				Voyage voyage(fichier_voyages[i], &ligne);
+			}
+			else {
+				throw logic_error("Aucune ligne trouvée pour le voyage");
+			}
+		}
+	}
+}
 
 int main() {
 
-	const std::vector<Voyage> voyages;
-
-	std::vector<std::vector<std::string>> fichier_voyages;
-	std::vector<std::vector<std::string>> fichier_lignes;
-	std::vector<std::vector<std::string>> fichier_stations;
-	std::vector<std::vector<std::string>> fichier_arrets;
+	const vector<Voyage> voyages;
 
 	lireFichier("RTC/trips.txt", fichier_voyages, delimiteur, true);
 	lireFichier("RTC/stops.txt", fichier_stations, delimiteur, true);
 	lireFichier("RTC/routes.txt", fichier_lignes, delimiteur, true);
 	lireFichier("RTC/stop_times.txt", fichier_arrets, delimiteur, true);
 
-
+	initVoyages();
 	return 0;
-}
-
-/* Construction de VOYAGE */
-void initVoyages(std::vector<Voyage> &voyages) {
-
-//	for (unsigned int i = 0; i < fichier_voyages.size(); i++) {
-//		Voyage voyage = new Voyage(voyages, Ligne* p_ligne);
-//	}
 }
